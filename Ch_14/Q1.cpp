0883a8780cd1e14644610e9d151521908ba09404
AnirudhChopra2810 <65032965+AnirudhChopra2810@users.noreply.github.com>
2020-06-04 14:53:56 +0530

    Ch_14_Q1_changes_added

--- Ch_14/Q1.cpp
+++ Ch_14/Q1.cpp
@@ -4,18 +4,18 @@ using namespace std;
 
 int main()
 {
-	int  n = 5, sequence[] = { 5, 5, 5, 5, 6 };
-	int flag = 0;
+	int  n = 6, sequence[] = { 5, 6, 5, 7, 6, 5 };
+	bool isPalindrome = true;
 
-	for (int i = 0; i < n; i++)
+	for (int i = 0; i < n / 2; i++)
 	{
-		if (sequence[i] == sequence[n - i - 1])
+		if (sequence[i] != sequence[n - i - 1])
 		{
-			flag = 1;
+			isPalindrome = false;
+			break;
 		}
-		else flag = 0;
 	}
 
-	if (flag == 1) cout << "No. is palindrome" << endl;
-	else if(flag == 0)  cout << "No. is not palindrome" << endl;
-} 
+	if (isPalindrome == true) cout << "Is palindrome" << endl;
+	else if (isPalindrome == false)  cout << "Not palindrome" << endl;
+}
